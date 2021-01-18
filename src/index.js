b54e676264db8b6c915fff99f3e1784745252b3b
ankurraj2606 <65525788+ankurraj2606@users.noreply.github.com>
2021-01-18 23:18:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,76 @@
+const mongoose = require("mongoose");
+const port = 3000;
+const app = require("./app");
+const marioModel = require("./models/marioChar");
+mongoose.connect("mongodb://localhost/testaroo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
-const mongoose = require('mongoose');
-const port = 3000
-const app = require('./app');
-//mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connection
+  .once("open", () => {
+    console.log("connection established");
+  })
+  .on("connectionError", (err) => {
+    console.log(err);
+  });
 
-// mongoose.connection.once('open', () =>{
-//     console.log('connection established')
-// }).on('connectionError',(err) =>{
-//     console.log(err);
-// })
+app.get("/mario", async (req, res) => {
+  const allMarios = await marioModel.find();
+  res.status(201).send(allMarios);
+});
+
+app.get("/mario:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const allMarios = await marioModel.find({ id });
+    res.status(201).send(allMarios);
+  } catch (err) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+app.post("/mario", async (req, res) => {
+  const mario = req.body;
+  console.log(mario);
+  try {
+    const newMario = new marioModel(mario);
+    await newMario.save();
+    res.status(201).send(newMario);
+  } catch (err) {
+    res.status(400).send({ message: "either name or weight is missing" });
+  }
+});
+
+app.patch("/mario:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const { mario } = req.body;
+  console.log(mario);
+  try {
+    const marioToUpd = await marioModel.findOne({ id });
+    //   marioToUpd.name = name;
+    //   marioToUpd.weight = weight;
+    marioToUpd.mario = mario;
+
+    await marioToUpd.save();
+    res.status(201).send(marioToUpd);
+  } catch (err) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+app.delete("/mario:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const marioToDel = await marioModel.delete({ id });
+    res.status(201).send({ message: "character deleted" });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //  Your code goes here
+const marioSchema = mongoose.Schema({
+  name: String,
+  weight: Number,
+  userId: mongoose.Schema.Types.ObjectId,
+});
 
+const marioModel = mongoose.model("mariochar", marioSchema);
 
 module.exports = marioModel;
